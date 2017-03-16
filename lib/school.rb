40ee9c004ad2be6ac540f55b9cba24123d973892
github username <github email address>
2017-03-16 19:41:32 -0400

    add school domain lab to github

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+require 'pry'
+
+class School
+
+	attr_accessor :grade, :student
+
+	def initialize(school)
+		@school = school
+		@roster = {}
+	end
+
+	def roster
+		@roster
+	end
+
+ 
+	def add_student(student, grade)
+		
+
+		if roster[grade] 
+			roster[grade] << student
+		else 
+			roster[grade] = []
+			roster[grade] << student
+
+		end
+	end
+
+
+	def grade(grade)
+		roster[grade]
+
+	end
+
+	def sort
+		@roster.each{ |k,v| v.sort! }
+	end
+end
